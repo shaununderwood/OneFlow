4c7e9a3184f6a96c31f47a0f42526cc2ac7d2e90
shaununderwood <shaun@masondigital.co.uk>
2016-02-16 07:47:51 +0000

    added promise code to obtain artistId and albums and peformed distinct operation

--- server/boot/script.js
+++ server/boot/script.js
@@ -1,6 +1,8 @@
 module.exports = function( app){
   'use strict';
 
+  var defer = require("promise").defer;
+
   // Load the spec from json doc
   let findArtistsIdSpec = require( './spotify-search-for-artist.json');
   let reqFindArtistId = restConnector.fromJSON( findArtistsIdSpec);
@@ -10,9 +12,36 @@ module.exports = function( app){
 
   let artistName = 'Iron Maiden';
 
-  let getArtistResponse = 
-  reqFindArtistId.invoke( { q: artistName});
+  // Get Artist Id
+  let artistResponseReceived = defer();
+  reqFindArtistId.invoke( { q: artistName}, ( data) => {
 
+    artistResponseReceived.resolve( data);
+  });
+
+  // Get Artist Albums
+  let albumResponseReceived = defer();
+  artistResponseReceived.promise.then( ( artistData ) => {
+
+      reqFindAlbums.invoke( { artistId: artistId}, ( data ) => {
+        albumResponseReceived.resolve( data);
+      });
+  });
+
+  // process and save Albums
+  albumResponseReceived.promise.then( ( albumData ) => {
+    let albums = { };
+
+    // filter out using a map to key album.name values
+    albumData.items.forEach( ( album ) => {
+      albums[ album.name] = album.name;
+    });
+
+    // save these to a database / flatfile
+    
+  });
 
 
 };
+
+
